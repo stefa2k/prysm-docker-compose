c66b1b23357bf4521ebb054da0076336cfebbbd1
Christoph Wagner <github@pandawhisperer.net>
2020-06-13 10:55:18 -0600

    Simplify for loop

--- createAccounts.sh
+++ createAccounts.sh
@@ -7,16 +7,14 @@ VALIDATORS=98    # Number of validators to create
 GAS_LIMIT=400000 # Max gas for funding transactio
 
 
-LASTVALIDATOR=$(($VALIDATORS-1))
-
-for i in $(seq 0 $LASTVALIDATOR); do
+for ((i=1; i<=$VALIDATORS; i++)) do
 	docker-compose -f create-account.yaml run validator-create-account > /tmp/validatorcreateoutput
 	RAWTXDATA=$(sed -n 8,1p /tmp/validatorcreateoutput | tr -d '\r')
 	# echo $RAWTXDATA
 	SEND_CMD="web3.eth.sendTransaction({ from: web3.eth.coinbase, to: '$BEACON_CONTRACT', value: web3.toWei(32, 'ether'), data: '${RAWTXDATA}', gas: $GAS_LIMIT })"
 	# echo "$SEND_CMD"
 	eval "docker exec -it geth geth attach http://localhost:8545/ --exec=\"$SEND_CMD\""
-	echo "sent successful, $seq"
+	echo "sent successful, $i"
 done
 
 rm /tmp/validatorcreateoutput
