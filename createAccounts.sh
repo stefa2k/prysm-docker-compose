75d40968ed8342bb7aa4b5b731ad727ab604013e
Christoph Wagner <github@pandawhisperer.net>
2020-06-13 10:46:15 -0600

    Updates for Onyx testnet

--- createAccounts.sh
+++ createAccounts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-BEACON_CONTRACT="0x5ca1e00004366ac85f492887aaab12d0e6418876"
+# BEACON_CONTRACT="0x5ca1e00004366ac85f492887aaab12d0e6418876" # Topaz testnet
+BEACON_CONTRACT="0x0F0F0fc0530007361933EaB5DB97d09aCDD6C1c8" # Onyx testnet
 
 VALIDATORS=98
 
@@ -10,7 +11,7 @@ LASTVALIDATOR=$(($VALIDATORS-1))
 
 for i in $(seq 0 $LASTVALIDATOR); do
 	docker-compose -f create-account.yaml run validator-create-account > /tmp/validatorcreateoutput
-	RAWTXDATA=$(sed -n 9,1p /tmp/validatorcreateoutput | tr -d '\r')
+	RAWTXDATA=$(sed -n 8,1p /tmp/validatorcreateoutput | tr -d '\r')
 	# echo $RAWTXDATA
 	SEND_CMD="web3.eth.sendTransaction({ from: web3.eth.coinbase, to: '$BEACON_CONTRACT', value: web3.toWei(32, 'ether'), data: '${RAWTXDATA}' })"
 	# echo "$SEND_CMD"
