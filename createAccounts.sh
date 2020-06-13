3b1a75b9154a46185587e02cd139ea821eb13900
Christoph Wagner <github@pandawhisperer.net>
2020-06-13 11:00:10 -0600

    Fixing typo

--- createAccounts.sh
+++ createAccounts.sh
@@ -4,7 +4,7 @@
 BEACON_CONTRACT="0x0F0F0fc0530007361933EaB5DB97d09aCDD6C1c8" # Onyx testnet
 
 VALIDATORS=98    # Number of validators to create
-GAS_LIMIT=400000 # Max gas for funding transactio
+GAS_LIMIT=400000 # Max gas for funding transactions
 
 
 for ((i=1; i<=$VALIDATORS; i++)) do
